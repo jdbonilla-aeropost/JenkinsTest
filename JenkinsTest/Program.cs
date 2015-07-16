7a8e0b10efde62e9be7b1dea8fad56014abeca74
jdbonilla-aeropost <jdbonilla@aeropost.com>
2015-07-16 13:52:01 -0600

    FirsBuild

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hola="hola mundo";
+            Console.WriteLine(hola);
+        }
+
+    }
+}
